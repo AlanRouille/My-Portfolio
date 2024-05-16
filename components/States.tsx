fe62ab057b645cf8a4d28a5b82d9ac303d58bb8a
Alan <rouille.alan@yahoo.fr>
2024-05-16 11:20:40 +0200

    ajout de la section Work

--- components/States.tsx
+++ components/States.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { Code, Ellipsis, LucideIcon, Scroll } from "lucide-react";
 import Link from "next/link.js";
 import { Section } from "./Section";
@@ -23,28 +24,45 @@ export const States = () => {
         </Card>
       </div>
       <div className="flex-[2] w-full flex flex-col gap-4">
-      <p className="text-lg text-muted-foreground">Work</p>
+      <Card className="p-4 flex-1">
+        <p className="text-lg text-muted-foreground">Work</p>
+            <div className="flex flex-col gap-4">
+              {WORKS.map((work, index) =>(
+                <Work
+                  key={index}
+                  image={work.image}
+                  title={work.title}
+                  role={work.role}
+                  date={work.date}
+                  url= {work.url}
+                  />
+              ))}
+            </div>
+      </Card>
         <Card className="p-4 flex-1">Contact me</Card>
       </div>
     </Section>
   );
 };
 
-const STATE_PROJECTS = [
+const STATE_PROJECTS : StateProjetProps [] = [
   {
     Logo: Scroll,
     title: "Patrick Betheuil",
-    description: "Website creation + Logo design"
+    description: "Website creation + Logo design",
+    url: "/",
   },
   {
     Logo: Code,
     title: "Marion Bertin photographe",
-    description: "Website creation + Logo design"
+    description: "Website creation + Logo design",
+    url: "/",
   },
   {
     Logo: Ellipsis,
     title: "Sky Picture",
-    description: "Website creation + Logo design"
+    description: "Website creation + Logo design",
+    url: "/",
   },
   
 ];
@@ -65,6 +83,38 @@ const StateProjet = (props: StateProjetProps) => {
       <div>
         <p className="text-lg font-semibold">{props.title}</p>
         <p className="text-sm text-muted-foreground">{props.description}</p>
+      </div>
+    </Link>
+  );
+};
+
+const WORKS :WorkProjetProps[] = [{
+  image: "https://scontent.fcdg3-1.fna.fbcdn.net/v/t39.30808-6/391589987_628114522824713_1843163460124779404_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=BzJ03ynF8ikQ7kNvgFHxE8V&_nc_ht=scontent.fcdg3-1.fna&oh=00_AYCOCRDjgF_T0ZRm5XEiwCom2V7vxiE1h8hFb2Wz_4LaaQ&oe=664B937F",
+  title:"Alan Rouillé",
+  role:"Founder",
+  date:"2023 - Present",
+  url:"https://www.facebook.com/AlanRouilleDesignDev",
+}] 
+
+type WorkProps = {
+  image: string;
+  title: string;
+  role: string;
+  date: string;
+  url: string;
+};
+
+const Work = (props: WorkProps) => {
+  return (
+    <Link href={props.url} className="inline-flex items-center gap-4 hover:bg-accent/50 transition-colors p-1 rounded">
+        <img src={props.image} alt={props.title} className="w-10 h-10 object-contain rounded-md"/>
+      
+      <div>
+        <p className="text-lg font-semibold">{props.title}</p>
+        <p className="text-sm text-muted-foreground">{props.role}</p>
+      </div>
+      <div className="ml-auto">
+      <p className="text-sm text-muted-foreground">{props.date}</p> 
       </div>
     </Link>
   );
